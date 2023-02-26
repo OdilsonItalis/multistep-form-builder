0bb89e3d7b54138849d9e88c7c7a3465d52147c7
OdilsonItalis <odilson.italis@gmail.com>
2023-02-26 13:40:14 -0500

    Created src folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
-import 'styles/main.css';
-import 'styles/chrome-bug.css';
+import '../styles/main.css';
+import '../styles/chrome-bug.css';
 import { useEffect, useState } from 'react';
 import React from 'react';
 
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { AppProps } from 'next/app';
-import { MyUserContextProvider } from 'utils/useUser';
+import { MyUserContextProvider } from '../utils/useUser';
 import type { Database } from 'types_db';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
